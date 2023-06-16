ef184bb1126c245908d723feb9abe8b88b0e0700
IbrahimTuzer <ibrahimtuzer12@gmail.com>
2023-06-16 20:48:24 +0300

    Görev 9 Katmanlı mimari

---
+++ AspNetMvcBlog/Blog.Business/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Business.Services;
+using Blog.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Blog.Business.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddBusinessServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>()/*(o =>
+            {
+                string connectionString = configuration.GetConnectionString("Default");
+                o.UseSqlServer(connectionString);
+            })*/;
+            services.AddTransient<PostService>();
+            services.AddTransient<CategoryService>();
+            services.AddTransient<PageService>();
+            services.AddTransient<SettingService>();
+            services.AddTransient<UserService>();
+            return services;
+        }
+        public static void EnsureDeletedAndCreated(IServiceScope scope)
+        {
+			// Veritabanı servisine erişim sağlar.
+			var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+			// Veritabanını sil
+			context.Database.EnsureDeleted();
+			// Veritabanını oluşturur
+			context.Database.EnsureCreated();
+
+			DbSeeder.Seed(context);
+		}
+    }
+}
---
+++ AspNetMvcBlog/Blog.Web.Mvc/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Business.Services;
+using Blog.Data;
+using Blog.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Mvc.ViewComponents;
+
+public class NavbarViewComponent : ViewComponent
+{
+	private readonly CategoryService _cs;
+	private readonly PageService _pageService;
+
+	public NavbarViewComponent(CategoryService cs,PageService pageService)
+	{
+		_cs = cs;
+		_pageService = pageService;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		//AppDbContext Db;
+		//db = Db;
+
+		return View(
+			new NavbarDto
+			{
+				categories = _cs.GetAll(),
+				pages = _pageService.GetAll()
+			}
+			);
+		//return View();
+	}
+}
